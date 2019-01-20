77073de3089575efe67e89619cf1afb8d07c0fc7
KevinYangKai <15000737431@163.com>
2019-01-21 00:16:21 +0800

    修改feign配置文件

--- feign-consumer/src/main/java/com/wonders/feignconsumer/controller/GetFromController.java
+++ feign-consumer/src/main/java/com/wonders/feignconsumer/controller/GetFromController.java
@@ -3,10 +3,12 @@ package com.wonders.feignconsumer.controller;
 import com.wonders.feignconsumer.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class GetFromController {
 
